d54accaa5dd45d355f24b171692292509e1ceff8
Victor Khov <khovtyv@gmail.com>
2022-04-05 22:08:10 +0700

    first commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,9 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var emojis = ["🚚", "✈️", "🏎", "⛵️", "🚂", "🚜", "🛺", "🚀", "🚙", "🚕", "🛵", "🚢", "🚁", "🏍", "🚲"]
+    @State var emojiCount = 3
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Memorize").font(.largeTitle)
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                    ForEach(emojis[0...emojis.count - 1], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            
+            HStack {
+                vehicles
+                Spacer()
+                bugs
+                
+            }.font(.largeTitle)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+    }
+        
+    
+    var vehicles: some View {
+        
+        Button(action: {
+            
+            let vehicles = ["🚚", "✈️", "🏎", "⛵️", "🚂", "🚜", "🛺", "🚀", "🚙", "🚕", "🛵", "🚢", "🚁", "🏍", "🚲"]
+            emojis = vehicles.shuffled()
+            
+        }, label: {
+            VStack {
+                Image(systemName: "car.fill")
+                Text("Vehicles").font(.body)
+            }
+        })
+        
+    }
+    
+    var bugs: some View {
+        
+        Button(action: {
+            
+            let bugs = ["🦟", "🐞", "🪲", "🐛", "🐜", "🕷", "🪳", "🪱", "🦗", "🪰", "🐌"]
+            emojis = bugs.shuffled()
+            
+            
+        }, label: {
+            VStack {
+                Image(systemName: "ladybug.fill")
+                Text("Bugs").font(.body)
+            }
+        })
+        
+    }
+    
+    
+}
+
+struct CardView: View {
+    var content : String
+    @State var isFacedUp : Bool = true
+    
+    var body: some View {
+        ZStack {
+            let card = RoundedRectangle(cornerRadius: 20)
+            if isFacedUp {
+                card.fill()
+                    .foregroundColor(.white)
+                card.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+                
+            } else {
+                
+                card.fill()
+                    .foregroundColor(.red)
+            }
+        }
+        .onTapGesture {
+            isFacedUp = !isFacedUp
+        }
     }
 }
 
